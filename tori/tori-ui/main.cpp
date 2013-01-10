d0c5082d84f09038b0db853dabdf23f17ebb84b3
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2013-01-10 19:54:45 -0300

    removing ui, and preparing tori-ui to work with Qt5

--- tori/tori-ui/main.cpp
+++ tori/tori-ui/main.cpp
@@ -1,24 +1,13 @@
-#include <QApplication>
-#include "qmlapplicationviewer.h"
+#include <QGuiApplication>
+#include <QQuickView>
 
-Q_DECL_EXPORT int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+    QGuiApplication app(argc, argv);
 
-    // UI Logic
-//    QObject *root = dynamic_cast<QObject *>(view.rootObject());
-//    QVariant msg = "Hello from C++";
-//    QVariant msg2 = "@tvstalker";
-//    QMetaObject::invokeMethod(root, "add_column",
-//    //             Q_RETURN_ARG(QVariant, returnedValue),
-//         Q_ARG(QVariant, msg), Q_ARG(QVariant, msg));
+    QQuickView view;
+    view.setSource(QUrl::fromLocalFile("qml/main.qml"));
+    view.show();
 
-    QScopedPointer<QApplication> app(createApplication(argc, argv));
-
-    QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/toriui/main.qml"));
-    viewer.setAttribute(Qt::WA_TranslucentBackground);
-    viewer.showExpanded();
-
-    return app->exec();
+    return app.exec();
 }
