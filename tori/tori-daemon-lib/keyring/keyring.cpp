1334d39135819f514ddaf9e2cd95764e714e15e9
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2013-01-28 12:20:10 -0300

    update copyright

--- tori/tori-daemon-lib/keyring/keyring.cpp
+++ tori/tori-daemon-lib/keyring/keyring.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (c) 2012 Manuel de la Pena <mandel@themacaque.com>
+ * Copyright (c) 2013 Manuel de la Pena <mandel@themacaque.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
